500ce7f5c5b580f0903c850f8402029609a8c531
Lynne West <thislynnewest@gmail.com>
2018-09-25 15:35:09 -0700

    added comments

--- numpyArray.py
+++ numpyArray.py
@@ -1,9 +1,11 @@
 import numpy as np
-height = [74, 67.5, 47, 30]
-weight = [230, 140, 48.8, 30]
+height = [74, 67.5, 47, 30] #in inches
+weight = [230, 140, 48.8, 30] #in lbs
 
+#change lists to numpy arrays
 np_height = np.array(height)
 np_weight = np.array(weight)
 
+#calculate bmi for each element in the lists
 bmi = 703*(np_weight/np_height**2)
 print(np.round(bmi,2))
