1364e796a6ebf7ee1b62e043476a7d91c26c6f53
Lynne West <thislynnewest@gmail.com>
2018-09-26 13:26:50 -0700

    Import data from an excel file and instert into 2D numpy array

---
+++ excel_import.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+hw = pd.read_excel("C:/Users/allbu/Documents/height_weight.xlsx")
+#put data in 2D numpy array
+hw_np = np.array(hw)
+#calculate bmi from 2D numpy array
+bmi=703*(hw_np[:,1]/hw_np[:,0]**2)
+print(bmi)
