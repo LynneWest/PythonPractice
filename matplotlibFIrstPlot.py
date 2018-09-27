ff334e3df394f38c2ad09ec8aa65b1b5e6d3744f
Lynne West <thislynnewest@gmail.com>
2018-09-27 11:43:21 -0700

    line plot with labels and title

--- matplotlibFIrstPlot.py
+++ matplotlibFIrstPlot.py
@@ -2,4 +2,9 @@ import matplotlib.pyplot as plt
 height = [74, 67.5, 47, 30] #in inches
 weight = [230, 140, 48.8, 30] #in lbs
 plt.plot(height, weight)
+plt.xlabel('height (inches)')
+plt.ylabel('weight')
+plt.title('heights and weights of the Jones family')
+plt.yticks([0,50,100,150,200,250],
+            ['0', '50 lbs', '100 lbs', '150 lbs', '200 lbs', '250 lbs'])
 plt.show()
