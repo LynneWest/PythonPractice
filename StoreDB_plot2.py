6f4230b8e1eb7b794755dc9c6268dd8f9194673e
Lynne West <thislynnewest@gmail.com>
2018-10-08 19:34:13 -0700

    Use pyodbc to connect to SQL Server in python and create bar graph

--- StoreDB_plot2.py
+++ StoreDB_plot2.py
@@ -11,14 +11,14 @@ conn_str = (
 cnxn = pyodbc.connect(conn_str)
 cur = cnxn.cursor()
 
-cur.execute("SELECT ITEM.Description, temp.TotalOrdered*UnitCost AS TotalSales FROM ITEM INNER JOIN (	SELECT Description, COUNT(OLI.ItemNumber) AS TotalOrdered FROM ITEM LEFT OUTER JOIN ORDER_LINE_ITEM AS OLI ON OLI.ItemNumber = ITEM.ItemNumber GROUP BY ITEM.Description) AS temp ON ITEM.Description = temp.Description")
+cur.execute("SELECT I.Description, temp.TotalOrdered*UnitCost AS TotalSales FROM ITEM AS I INNER JOIN (	SELECT Description, COUNT(OLI.ItemNumber) AS TotalOrdered FROM ITEM AS i LEFT OUTER JOIN ORDER_LINE_ITEM AS OLI ON OLI.ItemNumber = i.ItemNumber GROUP BY i.Description) AS temp ON I.Description = temp.Description")
 
 data = np.array(cur.fetchall())
 x = data[:,0]
 y = data[:,1]
-plt.bar(x,y, width=.5)
+plt.grid(axis='y')
+plt.bar(x,y, width=.5, color='g', zorder=2)
 plt.xticks(rotation='vertical')
 plt.yticks([0,10,20,30,40,50,60,70,80], ['0', '$10', '$20', '$30', '$40', '$50', '$60', '$70', '$80'])
-plt.title('Tool sale totals')
-plt.grid(axis='y')
+plt.title('Tool Sale Totals')
 plt.show()
